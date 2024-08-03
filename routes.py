b21777f4f4557cd6d4f38903e1ae1c804f3ca288
LeadsBuilds <dell50cent@gmail.com>
2024-08-03 07:26:25 -0300

    delete unused imports

--- routes.py
+++ routes.py
@@ -1,15 +1,6 @@
 from fasthtml.fastapp import *
 from src.pages.index import *
 
-from fasthtml.common import (
-    # These are the HTML components we use in this app
-    A, AX, Button, Card, Checkbox, Container, Div, Form, Grid, Group, H1, H2, Hidden, Input, Li, Main, Script, Style, Textarea, Title, Titled, Ul,
-    # These are FastHTML symbols we'll use
-    Beforeware, fast_app, SortableJS, fill_form, picolink, serve,
-    # These are from Starlette, Fastlite, fastcore, and the Python stdlib
-    FileResponse, NotFoundError, RedirectResponse, database, patch, dataclass
-)
-
 picocolorscss="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.colors.min.css";
 
 app, rt = fast_app(live=True, hdrs=(
